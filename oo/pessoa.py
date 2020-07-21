69ae1130f9dd94daa53f6f310388b40370fd6889
tassifreitas <tassianirfreitas@gmail.com>
2020-07-21 09:06:08 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,22 @@
 class Pessoa:
+    #atributo da classe
+    #métodos
+    def __init__(self, nome = None, idade=30):
+        #atributos de instancia ou atributos de objeto - atributos criado peelo __init__
+        #atributo nome com valor do parâmetro passado nome
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 if __name__ == '__main__':
-    p = Pessoa()
+    #p = Pessoa()
+    p = Pessoa('Tassiani')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Tassi'
+    print(p.nome)
+    print(p.idade)
 
