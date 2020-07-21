e2bf8c92c80c6094bf8e0807175c4031a54e4e3e
tassifreitas <tassianirfreitas@gmail.com>
2020-07-21 09:50:46 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,22 +1,27 @@
 class Pessoa:
     #atributo da classe
     #métodos
-    def __init__(self, nome = None, idade=30):
+    def __init__(self, *filhos, nome = None, idade=30):
         #atributos de instancia ou atributos de objeto - atributos criado peelo __init__
         #atributo nome com valor do parâmetro passado nome
         self.nome = nome
         self.idade = idade
+        #atributo complexo lista
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 if __name__ == '__main__':
     #p = Pessoa()
-    p = Pessoa('Tassiani')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Tassi'
-    print(p.nome)
-    print(p.idade)
-
+    #p = Pessoa('Tassiani')
+    tassiani = Pessoa(nome='Tassiani')
+    Mari = Pessoa(tassiani, nome='Mari')
+    print(Pessoa.cumprimentar(tassiani))
+    print(id(tassiani))
+    print(tassiani.cumprimentar())
+    print(Mari.nome)
+    print(Mari.idade)
+    for filho in Mari.filhos:
+        print(filho.nome)
+    #p.nome = 'Tassi'
+    #print(p.nome)
